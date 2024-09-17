b719f6558b84359bb6b7fd6a9ebcb199a957e162
Gowshi29 <gowshiganer@gmail.com>
2024-09-17 11:58:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer!","Designer!"],
+        strings: ["Designer!","Software Developer!","Quality Engineer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
